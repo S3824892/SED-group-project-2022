65cf2657e4bd3377a94ea93dbfead414109574dc
Phu Nguyen <57164905+s3802460@users.noreply.github.com>
2023-01-05 02:08:40 +0700

    updated

--- House.cpp
+++ House.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
-#include <string.h>
-using std::string;
-using std::cin;
-using std::cout;
+#include <string>
+#include <iomanip>
+#include <vector>
+using namespace std;
+#define FILE_NAME "House.dat"
 
 class House{
 protected:
@@ -13,48 +14,87 @@ protected:
     bool occupateStatus;
     int minScore;
 public:
-    House(string HName = "", string City = "", int minScore = 0,
-            int HouseRating = 0, bool occupated = false) 
-        : HName(HName), City(City), HouseRating(HouseRating), occupateStatus(occupateStatus),minScore(minScore){};
-
+    House();
     
-    int addHouse() {
-        std::fstream myfile;
-        myfile.open("House.dat", std::ios::out);
-        if (!myfile.is_open()) { 
-            cout << "Fail to create/open file \n";
-            return -1;
-        }else{
-            cout << std::endl << "INPUT HOUSE DETAILS" << std::endl;
-            cout << "HOUSE NAME: ";
-            getline(cin, HName);
-            myfile << HName;
-            cout << "CITY: ";
-            getline(cin, City);
-            myfile << City;
-            cout << "MINIMUM RATING: ";
-            cin >> minScore;
-            myfile << minScore;
-            occupateStatus = false;
-            myfile << occupateStatus;
-            myfile.close();
-        }
+    void readHouse() {
+        cout << endl << "INPUT HOUSE DETAILS" << endl;
+        cout << "House Name: ";
+        cin >> HName;
+        cout << "City: ";
+        cin >> City;
+        cout << "Occupation Status: ";
+        occupateStatus = false;
+        HouseRating = 5;
+        cout << "Minimum Score: ";
+        cin >> minScore;
     };
 
-    int removeHouse(){
-        std::fstream myfile;
-        myfile.open("House.dat", std::ios::out);
-        int choice = 0;
-        if (!myfile.is_open()) { 
-            cout << "Fail to create/open file \n";
-            return -1;
-        }else{
-            cout << "Do you want to unlist your house? \n";
-            cout << "1. Yes \n";
-            cout << "2. No \n";
-            cin >> choice;
-            if (int choice = 1){
-                cout << 
-            }
-    };
+    void displayHouse() {
+        cout << left << setw(10) << HName
+             << left << setw(10) << City
+             << left << setw(10) << occupateStatus
+             << left << setw(10) << HouseRating
+             << left << setw(10) << setprecision(5) << minScore << endl;
+    }
 };
+
+int writeToFile(House house, string filename) {
+    house.readHouse();
+    fstream file;
+    /// write to file
+    file.open(filename, ios::out|ios::binary|ios::app);
+    if (!file){
+        cout << "Error writing file" << endl;
+        return -1;
+    }
+    file.write((char*)&house,sizeof(house));
+    file.close();
+    cout << "SAVED YOUR HOUSE DETAILS" << endl;
+    return 0;
+}
+
+int readFromFile(string filename) {
+    House house;
+    fstream file;
+    file.open(filename,ios::in|ios::binary);
+    if (!file){
+        cout << "Error opening file" << endl;
+        return -1;
+    }
+    if (file.read((char*)&house,sizeof(house))) {
+        cout << endl
+             << left << setw(10) << "HOUSE NAME"
+             << left << setw(10) << "CITY"
+             << left << setw(10) << "OCCUPATION STATUS"
+             << left << setw(10) << "HOUSE RATING"
+             << left << setw(10) << "MINIMUM SCORE REQUIRED"  << endl;
+        while(!file.eof()) {
+            house.displayHouse();
+            file.read((char*)&house,sizeof(house));
+        }
+    }
+    else {
+        cout << "Error reading from file" << endl;
+        return -1;
+    }
+    file.close();
+    return 0;
+}
+
+int deleteFromFile(string filename){
+    House house;
+    string houseName;
+    cout << "Please input house name you want to delete: ";
+    cin >> houseName;
+    fstream file;
+    file.open(filename,ios::in|ios::binary);
+    if (!file){
+        cout << "Error opening file" << endl;
+        return -1;
+    }
+    vector <string> HName;
+    while (getline(file,houseName))
+        HName.push_back(houseName);
+    file.close();
+    return 0;
+}
--- Member.cpp
+++ Member.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <string.h>
-using std::string;
-using std::cout;
-using std::cin;
+#include <string>
+#include <iomanip>
+using namespace std;
+#define FILE_NAME "user.dat"
 
 class Member{
 protected:
     string username;
     string password;
-    int id;
     int creditPoint;
     int occupierScore;
     int phoneNumber;
     bool occupyStatus;
 
 public:
-    Member(string username = "", string password = "", int creditPoint = 0, 
+    Member(string username = "", string password = "", int creditPoint = 0,
             int occupierScore = 0, int phoneNumber = 0, bool occupyStatus = false) 
         : username(username), password(password), creditPoint(creditPoint), occupierScore(occupierScore),
            phoneNumber(phoneNumber), occupyStatus(occupyStatus){};
@@ -32,27 +31,28 @@ protected:
     
 public:
     User();
-    bool addMember(){
+    int addMember(){
         std::fstream myfile;
         myfile.open("user.dat", std::ios::out); 
         if (!myfile.is_open()) { 
             cout << "Fail to create/open file \n";
-            return false;
+            return -1;
         }else{//Get the username and password from console and save to file
 
-        //adding username
-        cout << "Enter your username: ";
-        cin >> username; //ignore previous '\n' entered by user
-        getline(cin, username); //read a line from console
-        myfile << username;
+            //adding username
+            cout << "Enter your username: ";
+            cin >> username; //ignore previous '\n' entered by user
+            getline(cin, username); //read a line from console
+            myfile << username;
 
-        //adding password
-        cout << "Enter the password: ";
-        cin >> password; //ignore previous '\n' entered by user
-        getline(cin, password); //read a line from console
-        myfile << username;
-        myfile.close();
-        cout << "Saved to the file! \n";
-        }   
+            //adding password
+            cout << "Enter the password: ";
+            cin >> password; //ignore previous '\n' entered by user
+            getline(cin, password); //read a line from console
+            myfile << username;
+            myfile.close();
+            cout << "Saved to the file! \n";
+            creditPoint += 500;
+        }  
     };
 };
