2540cb129d55ef044871b6f7a0c5acbce03560d7
Phu Nguyen <57164905+s3802460@users.noreply.github.com>
2023-01-04 13:27:37 +0700

    update

--- Member.cpp
+++ Member.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string.h>
 using std::string;
 using std::cout;
 using std::cin;
@@ -20,44 +21,38 @@ public:
             int occupierScore = 0, int phoneNumber = 0, bool occupyStatus = false) 
         : username(username), password(password), creditPoint(creditPoint), occupierScore(occupierScore),
            phoneNumber(phoneNumber), occupyStatus(occupyStatus){};
-    friend class System;
+    friend class User;
 };
 
-class System{
-private:
+
+
+class User : public Member{
+protected:
     std::vector <Member> members{};
+    
 public:
-    System{};
+    User();
     bool addMember(){
-            int id;
-            cout << "Input student ID: "; cin >> id;
-            int position = findMember(id); 
-            if ( position >= 0) { //Student is already in the system
-                cout << "Member has registered \n";
-                return false;
-            } else { //Add new Member
-                string username;
-                cout << "Input username: "; 
-                do {
-                    getline(cin, username);
-                } while (username == "");
-                members.push_back( Member(username, id) );
-                cout << "Input password: "; 
-                do {
-                    getline(cin, password);
-                } while (password == "");
-                members.push_back( Member(password, id) );
-                cout << "Added to the system successfully \n";
-                return true;
-            }
-        }
-        
-        int findMember(int id){
-            for (int i = 0; i < members.size(); i++){
-                if (members[i].id == id){
-                    return i; //return position if matched
-                }
-            }
-            return -1; //return -1 if does not matched
-        }
+        std::fstream myfile;
+        myfile.open("user.dat", std::ios::out); 
+        if (!myfile.is_open()) { 
+            cout << "Fail to create/open file \n";
+            return false;
+        }else{//Get the username and password from console and save to file
+
+        //adding username
+        cout << "Enter your username: ";
+        cin >> username; //ignore previous '\n' entered by user
+        getline(cin, username); //read a line from console
+        myfile << username;
+
+        //adding password
+        cout << "Enter the password: ";
+        cin >> password; //ignore previous '\n' entered by user
+        getline(cin, password); //read a line from console
+        myfile << username;
+        myfile.close();
+        cout << "Saved to the file! \n";
+        }   
+    };
 };
