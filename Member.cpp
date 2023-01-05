f7a2f4ed9db332bc7adb896a42321cd323140d44
S3824892 <89242569+S3824892@users.noreply.github.com>
2023-01-05 20:35:02 +0700

    Update Member.cpp

--- Member.cpp
+++ Member.cpp
@@ -54,6 +54,61 @@ public:
             cout << "Saved to the file! \n";
             creditPoint += 500;
         }
+    //Search
+    void SearchAvailableHouses(int member_id, int start_time, int end_time, string city){
+        // Check if the city is available
+        if (std::find(AVAILABLE_CITIES.begin(), AVAILABLE_CITIES.end(), city)== AVAILABLE_CITIES.end()){
+          cout << "Error: City is not available." << "\n";
+          return;
+       }
+        cout << "ID\tLocation\tDescription\tHouse Rating\tOwner ID" << "\n";
+        for (auto& h : houses_){
+          if (h.info().location()== city && h.is_available()&& h.owner_id()!= member_id &&
+              members_[member_id].credit_points()>= h.requestors().size()* (end_time - start_time + 1)&&
+              members_[member_id].occupier_rating()>= min_occupier_rating){
+            cout << h.id()<< "\t" << h.info().location()<< "\t" << h.info().description()<< "\t"
+                      << h.house_rating()<< "\t" << h.owner_id()<< "\n";
+         }
+       }
+    }
+
+    //Rate houses
+    void RateHouse(int occupier_id, int house_id, int score, string comment){
+      // Check if the occupier has occupied the house
+      if (std::find(houses_[house_id].occupiers().begin(), houses_[house_id].occupiers().end(), occupier_id)== houses_[house_id].occupiers().end()){
+        cout << "Error: Occupier has not occupied this house." << "\n";
+        return;
+     }
+    Review review;
+      review.id = reviews_.size();
+      review.house_id = house_id;
+      review.occupier_id = occupier_id;
+      review.score = score;
+      review.comment = comment;
+      reviews_.push_back(review);
+
+      // Update occupier rating
+      int total_score = 0;
+      int num_reviews = 0;
+      for (auto& r : reviews_){
+        if (r.occupier_id == occupier_id){
+          total_score += r.score;
+          num_reviews++;
+       }
+     }
+      members_[occupier_id].SetOccupierRating(total_score / num_reviews);
+
+      // Update house rating
+      total_score = 0;
+      num_reviews = 0;
+      for (auto& r : reviews_){
+        if (r.house_id == house_id){
+          total_score += r.score;
+          num_reviews++;
+       }
+     }
+      houses_[house_id].SetHouseRating(total_score / num_reviews);
+    }    
         return 0;
     }
 };
