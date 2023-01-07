21353737a8ab15b3b47c8029bb4112b343dc6b2d
S3824892 <89242569+S3824892@users.noreply.github.com>
2023-01-07 17:48:35 +0700

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -20,4 +20,30 @@ bool LogInAdmin() {
     return false;
   }
 }
+  
+// View all members in the system
+void ViewAllMembers() {
+  std::cout << "ID  | Name" << std::endl;
+  for (const auto& member : members_) {
+    std::cout << member.GetId() << " | " << member.GetName() << std::endl;
+  }
+}
+
+// View all houses in the system
+void ViewAllHouses() {
+  std::cout << "ID  | Location" << std::endl;
+  for (const auto& house : houses_) {
+    std::cout << house.GetId() << " | " << house.GetLocation() << std::endl;
+  }
+}
+
+// View all house listings in the system 
+void ViewAllHouseListings() {
+  std::cout << "ID  | House ID | Member ID | Start       | End         | Min Occupier Rating" << std::endl;
+  for (const auto& listing : listings_) {
+    std::cout << listing.GetId() << " | " << listing.GetHouseId() << " | " << listing.GetMemberId() << " | "
+              << listing.GetStart().ToString() << " | " << listing.GetEnd().ToString() << " | "
+              << listing.GetMinOccupierRating() << std::endl;
+  }
+}
 }
