d36dc62120e417b0b5d287cf679caa09fda9a4d6
Phu Nguyen <57164905+s3802460@users.noreply.github.com>
2023-01-08 21:25:30 +0700

    update new structure and start working on main

--- House.cpp
+++ House.cpp
@@ -1,100 +1,60 @@
+
+
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <iomanip>
-#include <vector>
+
+
+#include "House.h"
+
 using namespace std;
-#define FILE_NAME "House.dat"
 
-class House{
-protected:
-    string HName;
-    string City;
-    int HouseRating;
-    bool occupateStatus;
-    int minScore;
-public:
-    House();
-    
-    void readHouse() {
-        cout << endl << "INPUT HOUSE DETAILS" << endl;
-        cout << "House Name: ";
-        cin >> HName;
-        cout << "City: ";
-        cin >> City;
-        cout << "Occupation Status: ";
-        occupateStatus = false;
-        HouseRating = 5;
-        cout << "Minimum Score: ";
-        cin >> minScore;
-    };
+// Empty Constructor
+House::House(){}
 
-    void displayHouse() {
-        cout << left << setw(10) << HName
-             << left << setw(10) << City
-             << left << setw(10) << occupateStatus
-             << left << setw(10) << HouseRating
-             << left << setw(10) << setprecision(5) << minScore << endl;
-    }
-};
+// Destructor
+House::~House(){}
 
-int writeToFile(House house, string filename) {
-    house.readHouse();
-    fstream file;
-    /// write to file
-    file.open(filename, ios::out|ios::binary|ios::app);
-    if (!file){
-        cout << "Error writing file" << endl;
-        return -1;
-    }
-    file.write((char*)&house,sizeof(house));
-    file.close();
-    cout << "SAVED YOUR HOUSE DETAILS" << endl;
-    return 0;
+// Constructor
+House::House(
+    int id = 0,
+    string location = "",
+    string description = "",
+    double houseRating = 0,
+    double minOccupantScore = 0,
+    string startDate = "",
+    string endDate = "",
+    bool occupyStatus = false)
+{
+    this->id = id;
+    this->location = location;
+    this->description = description;
+    this->houseRating = houseRating;
+    this->minOccupantScore = minOccupantScore;
+    this->startDate = startDate;
+    this->endDate = endDate;
+    this->occupyStatus = occupyStatus;
 }
 
-int readFromFile(string filename) {
-    House house;
-    fstream file;
-    file.open(filename,ios::in|ios::binary);
-    if (!file){
-        cout << "Error opening file" << endl;
-        return -1;
-    }
-    if (file.read((char*)&house,sizeof(house))) {
-        cout << endl
-             << left << setw(10) << "HOUSE NAME"
-             << left << setw(10) << "CITY"
-             << left << setw(10) << "OCCUPATION STATUS"
-             << left << setw(10) << "HOUSE RATING"
-             << left << setw(10) << "MINIMUM SCORE REQUIRED"  << endl;
-        while(!file.eof()) {
-            house.displayHouse();
-            file.read((char*)&house,sizeof(house));
-        }
-    }
-    else {
-        cout << "Error reading from file" << endl;
-        return -1;
-    }
-    file.close();
-    return 0;
-}
+// GETTERS
+int House::getId() { return this->id; }
+string House::getLocation() { return this->location; }
+string House::getDescription() { return this->description; }
+double House::getHouseRating() { return this->houseRating; }
+double House::getMinOccupantScore() { return this->minOccupantScore; }
+string House::getStartDate() { return this->startDate; }
+string House::getEndDate() { return this->endDate; }
+bool House::getOccupyStatus() { return this->occupyStatus; }
 
-int deleteFromFile(string filename){
-    House house;
-    string houseName;
-    cout << "Please input house name you want to delete: ";
-    cin >> houseName;
-    fstream file;
-    file.open(filename,ios::in|ios::binary);
-    if (!file){
-        cout << "Error opening file" << endl;
-        return -1;
-    }
-    vector <string> HName;
-    while (getline(file,houseName))
-        HName.push_back(houseName);
-    file.close();
-    return 0;
-}
+// SETTERS
+void House::setId(int id) { this->id = id; }
+void House::setLocation(string location) { this->location = location; }
+void House::setDescription(string description) { this->description = description; }
+void House::setHouseRating(double houseRating) { this->houseRating = houseRating; }
+void House::setMinOccupantScore(double minOccupantScore) { this->minOccupantScore = minOccupantScore; }
+void House::setStartDate(string startDate) { this->startDate = startDate; }
+void House::setEndDate(string endDate) { this->endDate = endDate; }
+void House::setOccupyStatus(bool occupyStatus) { this->occupyStatus = occupyStatus; }
+
+// METHODS
+
+
